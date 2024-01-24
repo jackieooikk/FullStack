f3ef2d40e5ad61fdf20e7a57fc4bd3e364e84417
jackieooikk <98008869+jackieooikk@users.noreply.github.com>
2024-01-24 17:01:42 +1000

    completed module

--- 4.0 EJS/index.js
+++ 4.0 EJS/index.js
@@ -6,9 +6,20 @@ var app = express();
 var port = 3000;
 
 app.get("/", (req, res) => {
+    const today = new Date();
+    const day = today.getDay();
+
+    let type = "a weekday";
+    let adv = "it's time to work hard";
+
+    if (day == 0 || day == 6) {
+        type = "the weekend";
+        adv = "it's time to have some fun";
+    }
+
     res.render("index.ejs", {
-        daytype: "a weekday", 
-        advice: "it's time to work hard"
+        daytype: type, 
+        advice: adv,
     });
 }); 
 
